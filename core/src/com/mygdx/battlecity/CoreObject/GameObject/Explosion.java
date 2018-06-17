50c6111a85c41059a4fd5d846b2443b7e596a295
DESKTOP-GFJHOUI\15520394 <kienln394@gmail.com>
2018-06-17 17:34:57 +0700

    Cập nhật lại project

---
+++ core/src/com/mygdx/battlecity/CoreObject/GameObject/Explosion.java
@@ -0,0 +1,23 @@
+package com.mygdx.battlecity.CoreObject.GameObject;
+
+import com.mygdx.battlecity.CoreObject.Actor;
+import com.mygdx.battlecity.CoreObject.SpriteComponent;
+
+public class Explosion extends Actor {
+    public static final float EXPLOSION_TIME = 0.2f;
+    float explosionAccumTime = 0;
+
+    public Explosion(float x, float y) {
+        SetPosition(x, y);
+        AddComponent(new SpriteComponent("Explosion", 0.05f, true));
+    }
+
+    @Override
+    public void OnTick(float dt) {
+        super.OnTick(dt);
+        explosionAccumTime += dt;
+        if (explosionAccumTime >= EXPLOSION_TIME) {
+            Deactivate(this);
+        }
+    }
+}
